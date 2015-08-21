64575b06e5a445f621817c4c9dc44dc9a5f6cde2
codeRitter <codeRitter@users.noreply.github.com>
2015-08-20 22:36:30 -0400

    Fixed BeachBall_Monkey_Loader
    The link was bad. Now it isn't!

--- BeachBall_Monkey_Loader.js
+++ BeachBall_Monkey_Loader.js
@@ -13,7 +13,7 @@ setTimeout(loadBB, 5000);
 function loadBB() {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', 'http://codeRitter.github.io/BeachBall/BeachBall.js');
+    js.setAttribute('src', 'https://raw.githubusercontent.com/codeRitter/BeachBall/master/BeachBall.js');
     document.head.appendChild(js);
 }
 
