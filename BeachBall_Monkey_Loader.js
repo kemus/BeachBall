15fdc712dde1947a2cf9e1fc42cd1867d3c1414d
codeRitter <codeRitter@users.noreply.github.com>
2015-08-20 21:50:35 -0400

    Create BeachBall_Monkey_Loader.js
    Simple Greasemonkey/Tampermonkey script that grabs and loads BeachBall after 5 seconds.

---
+++ BeachBall_Monkey_Loader.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Beachball Loader
+// @namespace    http://github.com/codeRitter/BeachBall
+// @version      1.0
+// @description  Beachball autoloader for Sandcastle Builder.
+// @author       codeRitter
+// @match        http://castle.chirpingmustard.com/castle.html
+// @grant        none
+// ==/UserScript==
+
+setTimeout(loadBB, 5000);
+
+function loadBB() {
+    var js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('src', 'http://codeRitter.github.io/BeachBall/BeachBall.js');
+    document.head.appendChild(js);
+}
+
+// Xenko Beachball location - previous version.
+// http://xenko.github.io/BeachBall/BeachBall.js
