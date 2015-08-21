dce81e7db14be8214678b2f5ada13bb219675cee
codeRitter <codeRitter@users.noreply.github.com>
2015-08-21 12:53:23 -0400

    Update BeachBall_Monkey_Loader.js
    Fixed! Uses gh-pages / github.io now.

--- BeachBall_Monkey_Loader.js
+++ BeachBall_Monkey_Loader.js
@@ -13,7 +13,7 @@ setTimeout(loadBB, 5000);
 function loadBB() {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', 'https://raw.githubusercontent.com/codeRitter/BeachBall/master/BeachBall.js');
+    js.setAttribute('src', 'http://coderitter.github.io/BeachBall/BeachBall.js');
     document.head.appendChild(js);
 }
 
